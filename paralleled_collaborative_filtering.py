0ca1750cd1d085a380e7877b3d9c5ffae062b38c
canonrock16 <canonrock0401@gmail.com>
2021-08-30 09:34:09 +0900

    move methods to utils

--- paralleled_collaborative_filtering.py
+++ paralleled_collaborative_filtering.py
@@ -1,9 +1,10 @@
 import itertools
-
+import time
 import numpy as np
-import pandas as pd
+from utils import cos_sim,get_rate_matrix
 from mpi4py import MPI
-from surprise import Dataset, KNNBasic
+
+start = time.time()
 
 # 設定値
 core_num = 8
@@ -15,37 +16,6 @@ comm = MPI.COMM_WORLD  # 並列処理開始
 
 size = comm.Get_size()  # 並列処理に使用できるプロセッサ数
 rank = comm.Get_rank()  # 各プロセッサのIDのようなもの
-
-# コサイン類似度
-def cos_sim(v1, v2):
-    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-
-
-# ユーザー×アイテムの評価行列を作成
-def get_rate_matrix():
-    # データインポート
-    data = pd.read_table("ml-100k/u.data", header=None, usecols=[0, 1, 2], names=["user_id", "item_id", "rating"])
-
-    # ndarrayの行番号とユーザーID,列番号とアイテムIDを対応付ける辞書を作成
-    user_id2row_num = {}
-    row_num2user_id = {}
-    for i, user_id in enumerate(set(list(data["user_id"]))):
-        user_id2row_num[user_id] = i
-        row_num2user_id[i] = user_id
-
-    item_id2column_num = {}
-    column_num2item_id = {}
-    for i, item_id in enumerate(set(list(data["item_id"]))):
-        item_id2column_num[item_id] = i
-        column_num2item_id[i] = item_id
-
-    # ユーザー×アイテムの評価行列を作成
-    rate_matrix = np.zeros((len(user_id2row_num), len(item_id2column_num)))
-    # for row in tqdm(data.itertuples(), total=data.shape[0]):
-    for row in data.itertuples():
-        rate_matrix[user_id2row_num[row.user_id], item_id2column_num[row.item_id]] = row.rating
-
-    return rate_matrix, user_id2row_num, row_num2user_id, item_id2column_num, column_num2item_id
 
 
 # プロセス0が担当領域の分割と各プロセスへの送信、類似度計算結果の取得を行い、結果を収集してpredictする
@@ -81,6 +51,10 @@ if rank == 0:
             print(f"itemid:{column_num2item_id[i]},predicted_score:{topk_mean_ratings[i]}")
             break
 
+    elapsed_time = time.time() - start
+    print(f"elapsed_time:{elapsed_time}[sec]")
+
+
 else:
     # 各プロセスは担当の組み合わせの類似度を計算し、プロセス0へ返信
     rate_matrix = comm.recv(source=0,tag=0)
@@ -97,3 +71,4 @@ else:
     comm.send(sim_list, dest=0)
     print("Hello world {0} / {1}".format(rank, size))
     print(f'#{rank}/{size} complete!')
+
